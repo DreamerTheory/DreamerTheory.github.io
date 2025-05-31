13859fa83b5aca472953b040c2de709bb9b6e647
Rahul Linganagoudra <slinganagoudra@gmail.com>
2025-06-01 00:39:53 +0530

    change main.tsx and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/dreamertheory.github.io/',
+  base: './', // Changed from '/dreamertheory.github.io/'
   server: {    
     host: "::",
     port: 8080,
